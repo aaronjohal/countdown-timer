864b4edbaddf7ebca55488bb937c40ce04236ce6
Aaron Johal <Aaron.Johal@asglh-ml-23766.cable.virginm.net>
2021-02-23 16:54:36 +0000

    countdown timer complete

--- countdown-timer/countdown-timer/Controller/eventTimerVC.swift
+++ countdown-timer/countdown-timer/Controller/eventTimerVC.swift
@@ -6,24 +6,90 @@
 //
 
 import UIKit
+import SwiftUI
 
 class eventTimerVC: UIViewController {
+    
+    @IBOutlet weak var eventNameTxtField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var timepicker: UIDatePicker!
+    @IBOutlet weak var countdownTxt: UILabel!
+    @IBOutlet weak var countdownStack: UIStackView!
+    @IBOutlet weak var dayStack: UIStackView!
+    
+    
+    
+    @IBOutlet weak var daysLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
+    @IBOutlet weak var minutesLbl: UILabel!
+    @IBOutlet weak var secondsLbl: UILabel!
+    
+    
+    var timer: Timer!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
+        
+
     }
     
+    @IBAction func calcBtnPressed(_ sender: Any) {
+        if let eventName = eventNameTxtField.text {
+            if eventName == "" {
+              //put warning box here
+            } else {
+                timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true) //init timer
+                updateTime()
+                self.view.endEditing(true)
+               
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            }
+           
+        }
+        
+    
     }
-    */
+ 
+    
+  
+    
+  @objc func updateTime(){
 
+        let eventDate = datePicker.date
+        let todaysDate = Date()
+
+        let diffComponents = Calendar.current.dateComponents([.day, .hour, .minute,.second], from: todaysDate, to: eventDate) //get different components of the date e.g. hours, days  between two dates
+
+        let day = diffComponents.day!
+        let hours = diffComponents.hour!
+        let minutes = diffComponents.minute!
+        let seconds = diffComponents.second!
+    
+    
+    displayCountdown(days: day, hours: hours, minutes: minutes, seconds: seconds)
+        
+//        let timerLabel = "\(day)d \(hours)h \(minutes)m \(seconds)s"
+//        print(timerLabel)
+
+    }
+    
+    func displayCountdown(days: Int, hours: Int, minutes: Int, seconds: Int){
+        
+        countdownStack.isHidden = false
+        dayStack.isHidden = false
+        
+        daysLbl.text = "\(days)"
+        hoursLbl.text = "\(hours)"
+        minutesLbl.text = "\(minutes)"
+        secondsLbl.text = "\(seconds)"
+        
+    }
+
+   
+   
+    
+    
 }
--- countdown-timer/countdown-timer/View/customTextField.swift
+++ countdown-timer/countdown-timer/View/customTextField.swift
@@ -15,6 +15,7 @@ class customTextField: UITextField {
         let calendarBtn  = UIButton(type: .custom)
         calendarBtn.setImage(UIImage(systemName: "calendar"), for: .normal)
         calendarBtn.frame = CGRect(x: 5, y: (frame.size.height / 2) - 20 / 2, width: 20, height: 20)
+        calendarBtn.tintColor = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
         addSubview(calendarBtn)
         
     }
